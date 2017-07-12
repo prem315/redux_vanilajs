843a52e529a717da6b5cb21c87b6392901925d2d
Prem Patel <prem.patel3105@gmail.com>
2017-07-12 10:36:42 +0530

    added render function and actions

--- counter.js
+++ counter.js
@@ -14,7 +14,30 @@ function reducer(state, action){
       return state;
   }
 }
+// create a state
+var state = { counter: 0 }
 // create a store
-var store = Redux.store(reducer, state);
+var store = Redux.createStore(counter, state);
+
+var counterEl = document.getElementById('counter');
+// render function
+function render(){
+  var state = store.getState();
+  counterEl.innerHTML = state.count.toString();
+}
+// subscribe render function
+store.subscribe(render);
 
 // actions
+document.getElementById('add')
+  .addEventListener('click', function(){
+    store.dispatch({type: 'ADD'})
+  })
+document.getElementById('minus')
+  .addEventListener('click', function(){
+    store.dispatch({type: 'MINUS'})
+  })
+document.getElementById('reset')
+  .addEventListener('click', function(){
+    store.dispatch({type: 'RESET'})
+  })
